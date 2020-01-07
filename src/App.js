6434adbae7f5554e6b1722f9faa823184b37066d
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-01-07 18:40:35 +0200

    update readme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const App = () => (
-  <div className="flex h-screen justify-center items-center bg-red-200">
-    <p className="text-red-600 font-bold text-2xl">Hello world from react</p>
+  <div className="flex h-screen justify-center items-center bg-gray-900">
+    <p className="text-blue-600 font-bold text-5xl">Redux</p>
   </div>
 );
 
