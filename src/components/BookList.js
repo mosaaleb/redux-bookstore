9db1d14370666e0aa00768c465c89d9a8c3d1e5b
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-01-10 19:23:03 +0200

    Added bookList cmp and connect it to redux store

---
+++ src/components/BookList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Book from './Book';
+
+const mapStateToProps = (state) => ({ books: state.books });
+
+const BookList = ({ books }) => {
+  const renderBook = (book) => <Book key={book.id} book={book} />;
+
+  return (
+    <table className="table-auto">
+      <thead>
+        <tr>
+          <th className="px-4 py-2">Id</th>
+          <th className="px-4 py-2">Title</th>
+          <th className="px-4 py-2">Category</th>
+        </tr>
+      </thead>
+      <tbody>{books.map(renderBook)}</tbody>
+    </table>
+  );
+};
+
+BookList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+export default connect(mapStateToProps)(BookList);
